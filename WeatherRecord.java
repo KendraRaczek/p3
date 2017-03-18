1c4e551ee19a670c125ecb0b7d5c5e8ba3232280
micyang10 <yang363@wisc.edu>
2017-03-18 01:09:13 -0500

    Update WeatherRecord.java

--- WeatherRecord.java
+++ WeatherRecord.java
@@ -9,32 +9,46 @@ import java.util.Comparator;
 public class WeatherRecord extends Record{
     // TODO declare data structures required - WORKING
 	private int stationID;
+	private int numFiles;
 	private int date;
-	private ArrayList<Double> readings;
+	private double [] readings;
 
 	/**
 	 * Constructs a new WeatherRecord by passing the parameter to the parent constructor
 	 * and then calling the clear method()
 	 */
-    public WeatherRecord(int numFiles) {
-		super(numFiles);
-		clear();
-    }
+    	public WeatherRecord(int numFiles) {
+	    	super(numFiles);
+	    	readings = new double[numFiles];
+	    	clear();
+    	}
 	
 	/**
 	 * This comparator should first compare the stations associated with the given FileLines. If 
 	 * they are the same, then the dates should be compared. 
 	 */
-    private class WeatherLineComparator implements Comparator<FileLine> {
-		public int compare(FileLine l1, FileLine l2) {
-			// TODO implement compare() functionality - WORKING
+    	private class WeatherLineComparator implements Comparator<FileLine> {
+		
+	    	public int compare(FileLine l1, FileLine l2) {
+			String station1 = l1.getString().split(",")[0];
+			String station2 = l2.getString().split(",")[0];
+			String date1 = l1.getString().split(",")[1];
+			String date2 = l2.getString().split(",")[1];
 			
-			// compare stationID
-			
-			// compare date
-			
-			
-			return 0;
+			int stationInt1 = Integer.parseInt(station1);
+			int stationInt2 = Integer.parseInt(station2);
+			int dateInt1 = Integer.parseInt(date1);
+			int dateInt2 = Integer.parseInt(date2);
+							 
+			if(stationInt1 != stationInt2) {
+				return stationInt1 - stationInt2;
+			}
+			else if(dateInt1 != dateInt2) {
+				return date1 - date2;
+			}
+			else {
+				return 0;
+			}
 		}
 		
 		public boolean equals(Object o) {
@@ -46,17 +60,19 @@ public class WeatherRecord extends Record{
 	 * This method should simply create and return a new instance of the WeatherLineComparator
 	 * class.
 	 */
-    public Comparator<FileLine> getComparator() {
+    	public Comparator<FileLine> getComparator() {
 		return new WeatherLineComparator();
-    }
+    	}
 	
 	/**
 	 * This method should fill each entry in the data structure containing
 	 * the readings with Double.MIN_VALUE
 	 */
-    public void clear() {
-		// TODO initialize/reset data members
-    }
+    	public void clear() {
+		for (int i = 0; i < readings.length; i++) {
+			readings[i] = Double.MIN_VALUE;
+		}
+    	}
 
 	/**
 	 * This method should parse the String associated with the given FileLine to get the station, date, and reading
@@ -65,17 +81,32 @@ public class WeatherRecord extends Record{
 	 * this method will handle merging when this WeatherRecord is empty, the station and date associated with this
 	 * WeatherRecord should be set to the station and date values which were similarly parsed.
 	 */
-    public void join(FileLine li) {
-		// TODO implement join() functionality
-    	// use FileLine iterator
-    }
+    	public void join(FileLine li) {
+	    	String [] splitLine = li.getString.split(",");
+	    	stationID = Integer.parseInt(splitLine[0]);
+	    	date = Integer.parseInt(splitLine[1]);
+	    	readings[li.getFileIterator().getIndex()] = Double.parseDouble(splitLine[2]);
+    	}
 	
 	/**
 	 * See the assignment description and example runs for the exact output format.
 	 */
-    public String toString() {
-		// TODO
-		
-		return null;
-    }
+    	public String toString() {
+		String line = stationID + "," + date + ",";
+	    	for(int i = 0; i < readings.length; i++) {
+			if(readings[i] > Double.MIN_VALUE) {
+				line += readings[i] + ",";
+			}
+			else {
+				line += "-,";
+			}
+		}
+	    	if(readings[readings.length - 1] > Double.MIN_VALUE) {
+			line += readings[readings.length - 1];
+		}
+	    	else {
+			line += "-";
+		}
+	    	return line;
+    	}
 }
