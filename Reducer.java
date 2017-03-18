6fe297dfb90275d82ff73bbedf3dbc3e36ac42c8
micyang10 <yang363@wisc.edu>
2017-03-18 02:54:32 -0500

    Update Reducer.java

--- Reducer.java
+++ Reducer.java
@@ -1,46 +1,58 @@
+/////////////////////////////////////////////////////////////////////////////
+// Semester:         CS367 Spring 2017 
+// PROJECT:          Program 3
+// FILE:             Reducer.java
+//
+// TEAM:    Team 35 Java Badgers - P3
+// Authors: Michael Yang, Kendra Raczek
+// Author1: Michael Yang, yang363@wisc.edu, yang363, LEC 001
+// Author2: Kendra Raczek, raczek@wisc.edu, raczek, LEC 001
+//
+//////////////////////////// 80 columns wide //////////////////////////////////
+
 import java.io.*;
 import java.util.*;
 import java.lang.*;
 
 /**
- * Reducer solves the following problem: given a set of sorted input files (each
- * containing the same type of data), merge them into one sorted file. 
- *
+ * Reducer solves the following problem: given a set of sorted input files 
+ * (each containing the same type of data), merge them into one sorted file. 
  */
 public class Reducer {
-    // list of files for stocking the PQ
-    private List<FileIterator> fileList;
-    private String type,dirName,outFile;
+    	// list of files for stocking the PQ
+    	private List<FileIterator> fileList;
+    	private String type,dirName,outFile;
 
-    public static void main(String[] args) {
+    	public static void main(String[] args) {
 		if (args.length != 3) {
-			System.out.println("Usage: java Reducer <weather|thesaurus> <dir_name> <output_file>");
+			System.out.println("Usage: java Reducer <weather|" 
+					   + "thesaurus> <dir_name>" 
+					   +" <output_file>");
 			System.exit(1);
 		}
-
 		String type = args[0];
 		String dirName = args[1];
 		String outFile = args[2];
-
 		Reducer r = new Reducer(type, dirName, outFile);
 		r.run();
+    	}
 	
-    }
-
 	/**
-	 * Constructs a new instance of Reducer with the given type (a string indicating which type of data is being merged),
-	 * the directory which contains the files to be merged, and the name of the output file.
+	 * Constructs a new instance of Reducer with the given type (a string 
+	 * indicating which type of data is being merged), the directory which 
+	 * contains the files to be merged, and the name of the output file.
 	 */
-    public Reducer(String type, String dirName, String outFile) {
+    	public Reducer(String type, String dirName, String outFile) {
 		this.type = type;
 		this.dirName = dirName;
 		this.outFile = outFile;
-    }
+    	}
 
 	/**
-	 * Carries out the file merging algorithm described in the assignment description. 
+	 * Carries out the file merging algorithm described in the assignment 
+	 * description. 
 	 */
-    public void run() {
+    	public void run() {
 		File dir = new File(dirName);
 		File[] files = dir.listFiles();
 		Arrays.sort(files);
@@ -53,7 +65,8 @@ public class Reducer {
 		for (int i = 0; i < files.length; i++) {
 			File f = files[i];
 			if(f.isFile() && f.getName().endsWith(".txt")) {
-				fileList.add(new FileIterator(f.getAbsolutePath(), i));
+				fileList.add(new FileIterator
+					     (f.getAbsolutePath(), i));
 			}
 		}
 
@@ -68,8 +81,9 @@ public class Reducer {
 			System.out.println("Invalid type of data! " + type);
 			System.exit(1);
 		}
-		FileLinePriorityQueue fileQueue 
-			= new FileLinePriorityQueue(fileList.size(), r.getComparator());
+		
+		FileLinePriorityQueue fileQueue = new FileLinePriorityQueue
+				(fileList.size(), r.getComparator());
 		PrintWriter output = new PrintWriter(outFile);
 	    	try {
 			for (int = 0; i < fileList.size(); i++) {
@@ -81,9 +95,12 @@ public class Reducer {
 			while (!fileQueue.isEmpty()) {
 				FileLine file2 = fileQueue.removeMin();
 				if (file2.getFileIterator().hasNext()) {
-					fileQueue.insert(file2.getFileIterator().next());
+					fileQueue.insert(file2.
+							 getFileIterator().
+							 next());
 				}
-				if (r.getComparator().compare(file1, file2) == 0) {
+				if (r.getComparator().compare(file1, file2) 
+				    	== 0) {
 					r.join(file2);
 				}
 				else {
@@ -95,12 +112,14 @@ public class Reducer {
 			}
 			output.println(r.toString());
 			output.close();
+			
 		} catch (FileNotFoundException e) {
 			System.out.println("Error: File not found.");
 		} catch (PriorityQueueEmptyException e) {
-			System.out.println("Error: cannot remove from an empty queue.");
+			System.out.println("Error: cannot remove from"
+					   +" an empty queue.");
 		} catch (FileNotFoundException e) {
 			System.out.println("Error: queue is overfull");
 		}
-    }
+    	}
 }
