df0e58531ac83796866a7b102b3b3a1f5137c895
micyang10 <yang363@wisc.edu>
2017-03-19 18:54:35 -0500

    Update ThesaurusRecord.java

--- ThesaurusRecord.java
+++ ThesaurusRecord.java
@@ -18,27 +18,37 @@ import java.util.Comparator;
  * The ThesaurusRecord class is the child class of Record to be used when 
  * merging thesaurus data. The word field is the entry in the thesaurus, 
  * syn is the list of all associated synonyms.
+ * <p>Bugs: None that we are aware of
+ *
+ * @author Michael Yang, Kendra Raczek
  */
 public class ThesaurusRecord extends Record{
 	// word of thesaurus entry
 	private String word;
+	// number of files to be used for array of readings
+	private int numFiles;
 	// list of synonyms for specified word
 	private ArrayList<String> syn = new ArrayList<String>();
 	
 	/**
 	 * Constructs a new ThesaurusRecord by passing the parameter to the 
 	 * parent constructor and then calling the clear method()
+	 *
+	 * @parm numFiles: number of files to be used for array of readings
 	 */
 	public ThesaurusRecord(int numFiles) {
 		super(numFiles);
 		clear();
-	}
+	} //end of ThesaurusRecord(numFiles) constructor
 
 	/**
 	 * This Comparator should simply behave like the default(lexicographic)
 	 * compareTo() method for Strings, applied to the portions of the 
 	 * FileLines' Strings up to the ":". The getComparator() method of the 
 	 * ThesaurusRecord class will simply return an instance of this class.
+	 * <p>Bugs: None that we are aware of
+ 	 *
+ 	 * @author Michael Yang, Kendra Raczek
 	 */
 	private class ThesaurusLineComparator implements Comparator<FileLine> {
 		
@@ -58,12 +68,19 @@ public class ThesaurusRecord extends Record{
 			String[] line2 = l2.getString().split(":");
 			// compares first word of each line
 			return line1[0].compareTo(line2[0]);
-		}
+		} //end of compare(l1, l2) method
 		
+		/**
+		 * This method checks for whether or not an object is equal
+		 * to another object
+		 * 
+		 * @param o: the object that is being compared to
+		 */
 		public boolean equals(Object o) {
 			return this.equals(o);
-		}
-	}
+		} //end of equals(o) method
+		
+	} //end of ThesaurusLineComparator class
     
 	/**
 	 * This method should simply create and return a new instance of 
@@ -71,7 +88,7 @@ public class ThesaurusRecord extends Record{
 	 */
 	public Comparator<FileLine> getComparator() {
 		return new ThesaurusLineComparator();
-	}
+	} //end of getComparator() method
 	
 	/**
 	 * This method should (1) set the word to null and (2) empty the 
@@ -83,12 +100,14 @@ public class ThesaurusRecord extends Record{
 			syn.clear();
 		} catch (NullPointerException e) {	
 		}
-	}
+	} //end of clear() method
 	
 	/**
 	 * This method should parse the list of synonyms contained in the 
 	 * given FileLine and insert any which are not already found in this 
 	 * ThesaurusRecord's list of synonyms.
+	 *
+	 * @param w: the FileLine to be parsed
 	 */
 	public void join(FileLine w) {
 		// array containing word and list of synonyms
@@ -102,11 +121,13 @@ public class ThesaurusRecord extends Record{
 			}
 		}
 		Collections.sort(syn);
-	}
+	} //end of join(w) method
 	
 	/**
 	 * See the assignment description and example runs for the exact 
 	 * output format.
+	 * 
+	 * @return tRecord: a line of output to thesaurus_ouput.txt
 	 */
 	public String toString() {
 		String tRecord = word + ":";
@@ -116,5 +137,6 @@ public class ThesaurusRecord extends Record{
 		// accounts for last synonym without a following comma
 		tRecord += syn.get(syn.size() - 1);
 		return tRecord;
-	}
-}
+	} //end of toString() method
+	
+} //end of ThesaurusRecord class
