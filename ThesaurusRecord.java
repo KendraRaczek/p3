05c9edc4c1646ba05ef4459bf93590ded76fd341
micyang10 <yang363@wisc.edu>
2017-03-19 02:15:27 -0500

    Update ThesaurusRecord.java

--- ThesaurusRecord.java
+++ ThesaurusRecord.java
@@ -28,12 +28,12 @@ public class ThesaurusRecord extends Record{
 	 * Constructs a new ThesaurusRecord by passing the parameter to the 
 	 * parent constructor and then calling the clear method()
 	 */
-    	public ThesaurusRecord(int numFiles) {
+    public ThesaurusRecord(int numFiles) {
 		super(numFiles);
 		clear();
-    	}
+    }
 
-    	/**
+    /**
 	 * This Comparator should simply behave like the default(lexicographic)
 	 * compareTo() method for Strings, applied to the portions of the 
 	 * FileLines' Strings up to the ":". The getComparator() method of the 
@@ -50,56 +50,55 @@ public class ThesaurusRecord extends Record{
 		public boolean equals(Object o) {
 			return this.equals(o);
 		}
-    	}
+    }
     
 	/**
 	 * This method should simply create and return a new instance of 
 	 * the ThesaurusLineComparator class.
 	 */
-    	public Comparator<FileLine> getComparator() {
+    public Comparator<FileLine> getComparator() {
 		return new ThesaurusLineComparator();
-    	}
+    }
 	
 	/**
 	 * This method should (1) set the word to null and (2) empty the 
 	 * list of synonyms.
 	 */
-    	public void clear() {
-    		word = null;
-    		try {
+    public void clear() {
+    	word = null;
+    	try {
 			syn.clear();
-		} catch (NullPointerException e) {
-			
+		} catch (NullPointerException e) {	
 		}
-    	}
+    }
 	
 	/**
 	 * This method should parse the list of synonyms contained in the 
 	 * given FileLine and insert any which are not already found in this 
 	 * ThesaurusRecord's list of synonyms.
 	 */
-    	public void join(FileLine w) {
-	    	String[] fileSplit = w.getString().split(":");
-	    	word = fileSplit[0];
-	    	String[] synArray = fileSplit[1].split(",");
-	    	for (int i = 0; i < synArray.length; i++) {
-		    	if (!syn.contains(synArray[i])) {
-			    	syn.add(synArray[i]);
-		    	}
-	    	}
-	    	Collections.sort(syn);
-    	}
+    public void join(FileLine w) {
+	    String[] fileSplit = w.getString().split(":");
+	    word = fileSplit[0];
+	    String[] synArray = fileSplit[1].split(",");
+	    for (int i = 0; i < synArray.length; i++) {
+		    if (!syn.contains(synArray[i])) {
+			    syn.add(synArray[i]);
+		    }
+	    }
+	    Collections.sort(syn);
+    }
 	
 	/**
 	 * See the assignment description and example runs for the exact 
 	 * output format.
 	 */
-    	public String toString() {
-    		String tRecord = word + ":";
-    		for (int i = 0; i < syn.size() - 1; i++) {
-				tRecord += syn.get(i) + ",";
+    public String toString() {
+    	String tRecord = word + ":";
+    	for (int i = 0; i < syn.size() - 1; i++) {
+			tRecord += syn.get(i) + ",";
 		}
-    		tRecord += syn.get(syn.size() - 1);
+    	tRecord += syn.get(syn.size() - 1);
 		return tRecord;
 	}
 }
